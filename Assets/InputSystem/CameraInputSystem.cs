2d0bfaa87485e0e0cd40c74d8cf6e143907ec468
Nesquiko <lukas.castven@gmail.com>
2025-10-16 20:37:34 +0200

    Slightly less scuffed camera

--- Assets/InputSystem/CameraInputSystem.cs
+++ Assets/InputSystem/CameraInputSystem.cs
@@ -1,30 +1,48 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine.Assertions;
 
 public class CameraInputSystem : MonoBehaviour
 {
 
-    private const float MOVE_SPEED = 75f;
-    private const float ROTATE_SPEED = 75f;
-    private const float MOUSE_DRAG_MOVE_COEF = .5f;
-    private const float ZOOM_COEF = 4f;
-    private const float ZOOM_SPEED = 35f;
-    private const float ZOOM_MIN = 5f;
-    private const float ZOOM_MAX = 200f;
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 75f;
+    [SerializeField] private float rotateSpeed = 75f;
+    [SerializeField] private float mouseDragMoveCoef = .5f;
 
-    private const float MAP_MIN_X = 100f;
-    private const float MAP_MAX_X = 400f;
-    private const float MAP_MIN_Z = 100f;
-    private const float MAP_MAX_Z = 400f;
+    [Header("Zoom")]
+    [SerializeField, Range(0f, 1f)] private float zoomT = 0f;
+    [SerializeField] private float zoomSpeed = 1.5f;
+    [SerializeField] private Vector3 offsetFar = new(0f, 65f, -170f);
+    [SerializeField] private Vector3 offsetNear = new(0f, 5f, -10f);
+    [SerializeField] private float offsetSmoothTime = 0.15f;
+    private Vector3 currentOffset;
+    private Vector3 offsetVelocity;
+
+    [Header("Map bounds")]
+    [SerializeField] private float mapMinX = 100f;
+    [SerializeField] private float mapMaxX = 400f;
+    [SerializeField] private float mapMinZ = 100f;
+    [SerializeField] private float mapMaxZ = 400f;
+
 
     private CameraInputActions cameraInputActions;
 
     [SerializeField] private CinemachineCamera cinemachineCamera;
+    private CinemachineFollow cinemachineFollow;
 
     private void Awake()
     {
         cameraInputActions = new CameraInputActions();
         cameraInputActions.Camera.Enable();
+    }
+
+    void Start()
+    {
+        cinemachineFollow = cinemachineCamera.GetComponent<CinemachineFollow>();
+        Assert.IsNotNull(cinemachineFollow);
+        currentOffset = EvaluateOffset(zoomT);
+        cinemachineFollow.FollowOffset = currentOffset;
     }
 
     private void FixedUpdate()
@@ -37,30 +55,35 @@ public class CameraInputSystem : MonoBehaviour
         if (cameraInputActions.Camera.MouseShouldDrag.IsPressed())
         {
             var mouseDelta = cameraInputActions.Camera.MouseDragMovement.ReadValue<Vector2>();
-            inputMoveVec3.x = -(mouseDelta.x * MOUSE_DRAG_MOVE_COEF);
-            inputMoveVec3.z = -(mouseDelta.y * MOUSE_DRAG_MOVE_COEF);
+            inputMoveVec3.x = -(mouseDelta.x * mouseDragMoveCoef);
+            inputMoveVec3.z = -(mouseDelta.y * mouseDragMoveCoef);
         }
-
 
         var move = transform.forward * inputMoveVec3.z + transform.right * inputMoveVec3.x;
-        var newPos = transform.position + move * MOVE_SPEED * Time.deltaTime;
-        newPos.x = Mathf.Clamp(newPos.x, MAP_MIN_X, MAP_MAX_X);
-        newPos.z = Mathf.Clamp(newPos.z, MAP_MIN_Z, MAP_MAX_Z);
+        var newPos = transform.position + move * moveSpeed * Time.deltaTime;
+        newPos.x = Mathf.Clamp(newPos.x, mapMinX, mapMaxX);
+        newPos.z = Mathf.Clamp(newPos.z, mapMinZ, mapMaxZ);
         transform.position = newPos;
 
-        // Keyboar rotation
+        // Keyboard rotation
         var rotate = cameraInputActions.Camera.Rotate.ReadValue<float>();
-        transform.eulerAngles += new Vector3(0, rotate * ROTATE_SPEED * Time.deltaTime, 0);
+        transform.eulerAngles += new Vector3(0, rotate * rotateSpeed * Time.deltaTime, 0);
 
         // Zoom
-        var zoom = cameraInputActions.Camera.Zoom.ReadValue<float>();
-        if (zoom != 0)
+        var zoomInput = cameraInputActions.Camera.Zoom.ReadValue<float>();
+        if (Mathf.Abs(zoomInput) > Mathf.Epsilon)
         {
-            var startZoom = cinemachineCamera.Lens.OrthographicSize;
-            var endZoom = startZoom + (zoom * ZOOM_COEF);
-            var lerped = Mathf.Lerp(startZoom, endZoom, Time.deltaTime * ZOOM_SPEED);
-            cinemachineCamera.Lens.OrthographicSize = Mathf.Clamp(lerped, ZOOM_MIN, ZOOM_MAX);
+            zoomT = Mathf.Clamp01(zoomT - zoomInput * zoomSpeed * Time.deltaTime);
         }
+
+        var targetOffset = EvaluateOffset(zoomT);
+        currentOffset = Vector3.SmoothDamp(currentOffset, targetOffset, ref offsetVelocity, offsetSmoothTime);
+        cinemachineFollow.FollowOffset = currentOffset;
+    }
+
+    private Vector3 EvaluateOffset(float t)
+    {
+        return Vector3.LerpUnclamped(offsetFar, offsetNear, t);
     }
 
     private void OnDrawGizmos()
@@ -68,10 +91,10 @@ public class CameraInputSystem : MonoBehaviour
         Gizmos.color = Color.yellow;
         float y = transform.position.y;
 
-        Vector3 p1 = new Vector3(MAP_MIN_X, y, MAP_MIN_Z);
-        Vector3 p2 = new Vector3(MAP_MAX_X, y, MAP_MIN_Z);
-        Vector3 p3 = new Vector3(MAP_MAX_X, y, MAP_MAX_Z);
-        Vector3 p4 = new Vector3(MAP_MIN_X, y, MAP_MAX_Z);
+        Vector3 p1 = new Vector3(mapMinX, y, mapMinZ);
+        Vector3 p2 = new Vector3(mapMaxX, y, mapMinZ);
+        Vector3 p3 = new Vector3(mapMaxX, y, mapMaxZ);
+        Vector3 p4 = new Vector3(mapMinX, y, mapMaxZ);
 
         Gizmos.DrawLine(p1, p2);
         Gizmos.DrawLine(p2, p3);
