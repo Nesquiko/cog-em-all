04c086bc4f3be2aa9383f3ce2b81d0d50196750f
Michal-Kilian <kilian.michal.kili@gmail.com>
2025-12-02 17:51:18 +0100

    wip: skill tree

--- Assets/Scripts/UI/SkillTreeConnector.cs
+++ Assets/Scripts/UI/SkillTreeConnector.cs
@@ -4,73 +4,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[Serializable]
-public class Connection
-{
-    public RectTransform from;
-    public RectTransform to;
-}
-
 public class SkillTreeConnector : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private RectTransform treeContainer;
-    [SerializeField] private RectTransform connectionLayer;
-    [SerializeField] private GameObject connectionPrefab;
-    [SerializeField] private string linePrefix;
+    [SerializeField] private RectTransform[] ranks;
 
-    [SerializeField] private List<Connection> connections = new();
-
-    private void Start()
-    {
-        StartCoroutine(DeferredGeneration());
-    }
-
-    private IEnumerator DeferredGeneration()
-    {
-        yield return null;
-        GenerateConnections();
-    }
-
-    [ContextMenu("Generate Lines")]
+    [ContextMenu("Generate Connections")]
     public void GenerateConnections()
     {
-        ClearConnections();
-
-        foreach (var c in connections)
+        foreach (var rank in ranks)
         {
-            if (c.from == null || c.to == null) continue;
-            DrawConnection(c.from, c.to);
+            for (int i = rank.childCount - 1; i >= 0; i--)
+            {
+                var node = rank.GetChild(i);
+                node.GetComponent<SkillTreeNodeButton>().GenerateConnections();
+            }
         }
     }
 
-    private void DrawConnection(RectTransform from, RectTransform to)
-    {
-        var lineObj = Instantiate(connectionPrefab, connectionLayer);
-        lineObj.name = $"{linePrefix}_{from.name}_{to.name}";
-
-        var line = lineObj.GetComponent<Image>();
-        var rt = line.rectTransform;
-
-        Vector2 start = connectionLayer.InverseTransformPoint(from.position);
-        Vector2 end = connectionLayer.InverseTransformPoint(to.position);
-
-        Vector2 direction = end - start;
-        float length = direction.magnitude;
-
-        rt.sizeDelta = new(length, 4f);
-        rt.anchoredPosition = start + direction * 0.5f;
-        rt.rotation = Quaternion.Euler(0, 0,
-            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-    }
-
+    [ContextMenu("Clear Connections")]
     private void ClearConnections()
     {
-        for (int i = connectionLayer.childCount - 1; i >= 0; i--)
+        foreach (var rank in ranks)
         {
-            var c = connectionLayer.GetChild(i);
-            if (c.name.StartsWith(linePrefix))
-                DestroyImmediate(c.gameObject);
+            for (int i = rank.childCount - 1; i >= 0; i--)
+            {
+                var node = rank.GetChild(i);
+                node.GetComponent<SkillTreeNodeButton>().ClearConnections();
+            }
         }
     }
 
--- Assets/Scripts/UI/SkillTreeNodeButton.cs
+++ Assets/Scripts/UI/SkillTreeNodeButton.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,34 +11,77 @@ public enum SkillNodeType
     FactionSpecific,
 }
 
+public enum SkillNodeState
+{
+    Locked = 0,
+    Unlocked = 1,
+    Active = 2,
+}
+
 public class SkillTreeNodeButton : MonoBehaviour
 {
+    [SerializeField] private string skillSlug;
+    [SerializeField] private Faction faction;
     [SerializeField] private Button button;
+    [SerializeField] private GameObject unlockedOverlay;
     [SerializeField] private GameObject lockedOverlay;
     [SerializeField] private Image borderImage;
-    [SerializeField] private bool unlocked = true;
+    [SerializeField] private SkillNodeState state;
     [SerializeField] private SkillNodeType type;
-    [SerializeField] private Faction faction;
+    [SerializeField] private RectTransform rectTransform;
+    [SerializeField] private SkillTreeNodeButton[] prerequisities;
+    [SerializeField] private ModifiersDatabase modifiersDatabase;
+    [SerializeField] private GameObject connectionPrefab;
+    [SerializeField] private RectTransform connectionLayer;
+    [SerializeField] private string linePrefix;
 
-    public bool Unlocked
+    Modifier modifier;
+
+    public RectTransform RectTransform => rectTransform;
+
+    public SkillNodeState State
     {
-        get => unlocked;
+        get => state;
         set
         {
-            unlocked = value;
+            state = value;
             UpdateVisual();
         }
     }
 
     private void Awake()
     {
+        modifier = GetModifier();
         UpdateVisual();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DeferredConnection());
+    }
+
+    private Modifier GetModifier()
+    {
+        return (type) switch
+        {
+            SkillNodeType.Minor => modifiersDatabase.GetGenericMinorModifierBySlug(skillSlug),
+            SkillNodeType.Major => modifiersDatabase.GetGenericMajorModifierBySlug(skillSlug),
+            SkillNodeType.FactionSpecific => (faction) switch
+            {
+                Faction.TheBrassArmy => modifiersDatabase.GetTheBrassArmyModifierBySlug(skillSlug),
+                Faction.TheValveboundSeraphs => modifiersDatabase.GetValveboundSeraphsModifierBySlug(skillSlug),
+                Faction.OverpressureCollective => modifiersDatabase.GetOverpressureCollectiveModifierBySlug(skillSlug),
+                _ => null,
+            },
+            _ => null,
+        };
     }
 
     private void UpdateVisual()
     {
-        lockedOverlay.SetActive(!unlocked);
-        button.interactable = unlocked;
+        lockedOverlay.SetActive(state == SkillNodeState.Locked);
+        unlockedOverlay.SetActive(state == SkillNodeState.Unlocked);
+        button.interactable = state != SkillNodeState.Locked;
 
         if (type == SkillNodeType.FactionSpecific)
             borderImage.color = FactionAccent(faction);
@@ -50,5 +96,51 @@ public class SkillTreeNodeButton : MonoBehaviour
             Faction.OverpressureCollective => Color.yellow,
             _ => Color.black,
         };
+    }
+
+    private IEnumerator DeferredConnection()
+    {
+        yield return null;
+        GenerateConnections();
+    }
+
+    [ContextMenu("Generate Connections")]
+    public void GenerateConnections()
+    {
+        foreach (var pr in prerequisities)
+        {
+            DrawConnection(rectTransform, pr.RectTransform);
+        }
+    }
+
+    private void DrawConnection(RectTransform from, RectTransform to)
+    {
+        var lineObj = Instantiate(connectionPrefab, connectionLayer);
+        lineObj.name = $"{linePrefix}_{from.name}_{to.name}";
+
+        var line = lineObj.GetComponent<Image>();
+        var rt = line.rectTransform;
+
+        Vector2 start = connectionLayer.InverseTransformPoint(from.position);
+        Vector2 end = connectionLayer.InverseTransformPoint(to.position);
+
+        Vector2 direction = end - start;
+        float length = direction.magnitude;
+
+        rt.sizeDelta = new(length, 4f);
+        rt.anchoredPosition = start + direction * 0.5f;
+        rt.rotation = Quaternion.Euler(0, 0,
+            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+    }
+
+    [ContextMenu("Clear Connections")]
+    public void ClearConnections()
+    {
+        for (int i = connectionLayer.childCount - 1; i >= 0; i--)
+        {
+            var c = connectionLayer.GetChild(i);
+            if (c.name.StartsWith(linePrefix))
+                DestroyImmediate(c.gameObject);
+        }
     }
 }
